4e1248966aad172ffc1d27b655c11d8f6d86ffa2
opuerto <oscar.puertohn@gmail.com>
2018-05-22 21:43:59 +0000

    end of section 8

--- src/features/event/EventDashbord/EventDashboard.jsx
+++ src/features/event/EventDashbord/EventDashboard.jsx
@@ -1,89 +1,36 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
-import { Grid, Button } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
-import EventForm from "../eventForm/EventForm";
-import cuid from 'cuid';
-import { createEvent, deleteEvent, updateEvent } from '../eventActions'
+import {  deleteEvent } from '../eventActions'
 
 const mapState = (state) => ({
   events:state.events
 })
 
 const actions = {
-    createEvent,
-    deleteEvent,
-    updateEvent
+    deleteEvent
 }
 
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  
+    
   //An alternative method to this.handleFormOpen = this.handleFormOpen.bind(this); functions is to use this arrow function doing this
   // handleFormOpen = () => avoid having to declare and bind the function on the constructor
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null, 
-      isOpen: true
-    });
-  };
-
-  handleOpenEvent = (eventToOpen) => () => {
-      this.setState({
-        selectedEvent: eventToOpen,
-        isOpen:true
-      })
-      console.log(this.state.selectedEvent)
-  }
-
-  handleCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
-  handleCreateEvent = (newEvent) => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL = 'assets/user.png';
-    this.props.createEvent(newEvent)
-    this.setState({
-        isOpen:false
-    })
-
-  }
-
-  handleUpdateEvent = (updatedEvent) => {
-    
-    this.props.updateEvent(updatedEvent)
-    this.setState({
-      isOpen:false,
-      selectedEvent: null
-    })
-  }
+ 
 
   handleDeleteEvent = (eventId) => () => {
     this.props.deleteEvent(eventId);
   }
 
   render() {
-    const {selectedEvent} = this.state;
     const {events} = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={events} />
+          <EventList deleteEvent={this.handleDeleteEvent}  events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
-          <Button
-            onClick={this.handleFormOpen}
-            positive
-            content="Create Event"
-          />
-          {this.state.isOpen && <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
+          
         </Grid.Column>
       </Grid>
     );
--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
+import {connect} from 'react-redux'
+import { Grid } from "semantic-ui-react";
+import EventDetailedHeader from "./EventDetailedHeader";
+import EventDetailedChat from "./EventDetailedChat";
+import EventDetailedInfo from "./EventDetailedInfo";
+import EventDetailedSideBar from "./EventDetailedSideBar";
 
-const EventDetailedPage = () => {
-  return (
-    <div>
-      <h1>Event Detailed page</h1>
-    </div>
-  )
+const mapState = (state, ownProps) => {
+    //get the parameters of the url from ownProps 
+    const eventId = ownProps.match.params.id;
+
+    // we use this empty event to avoid possible errors in case you write the wrong id in the url 
+    // it will just display nothing instead of an error 
+    let event = {};
+
+    if(eventId && state.events.length > 0) {
+      event = state.events.filter(event => event.id === eventId)[0];
+    }
+    return {
+      event
+    }
 }
 
-export default EventDetailedPage
+const EventDetailedPage = ({event}) => {
+  return (
+    
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailedHeader event={event} />
+        <EventDetailedInfo event={event} />
+        <EventDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <EventDetailedSideBar attendees={event.attendees} />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
+export default connect(mapState)(EventDetailedPage);
